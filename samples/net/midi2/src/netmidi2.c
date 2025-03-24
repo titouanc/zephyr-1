79085294dc6d2b4859c29a6a5e2ff9be18c3af84
Titouan Christophe <moiandme@gmail.com>
2025-03-24 11:41:25 +0100

    Use net_bufs for readability
    Signed-off-by: Titouan Christophe <moiandme@gmail.com>

--- samples/net/midi2/src/netmidi2.c
+++ samples/net/midi2/src/netmidi2.c
@@ -5,50 +5,63 @@ LOG_MODULE_REGISTER(udp_midi, LOG_LEVEL_DBG);
 
 #define BUFSIZE 256
 
-static int udp_midi_dispatch_command_packet(struct udp_midi_ep *ep, const uint8_t *cmd_pkt, size_t cmd_pkt_len, uint8_t *reply, size_t reply_len)
+static int udp_midi_dispatch_command_packet(struct udp_midi_ep *ep,
+					    struct net_buf_simple *rx,
+					    struct net_buf_simple *tx)
 {
 	struct midi_ump ump;
-	const uint32_t *words;
+	size_t payload_len;
+	uint8_t cmd_code, payload_len_words;
+	uint16_t cmd_data;
 
-	switch (cmd_pkt[0]) {
+	if (rx->len < 4) {
+		LOG_ERR("Incomplete UDP MIDI command packet header");
+		return -1;
+	}
+
+	cmd_code = net_buf_simple_pull_u8(rx);
+	payload_len_words = net_buf_simple_pull_u8(rx);
+	payload_len = 4 * payload_len_words;
+	cmd_data = net_buf_simple_pull_be16(rx);
+
+	if (payload_len > rx->len) {
+		LOG_ERR("Incomplete UDP MIDI command packet payload");
+		return -1;
+	}
+
+	switch (cmd_code) {
 	case COMMAND_PING:
-		if (cmd_pkt_len != 8) {
-			LOG_ERR("Invalid PING packet length (%d)", cmd_pkt_len);
-			return -EINVAL;
+		if (payload_len_words != 1) {
+			LOG_ERR("Invalid payload length for PING packet");
+			return -1;
 		}
-
-		if (reply_len < 8) {
-			LOG_WRN("Not enough buffer space for PING_REPLY");
-			return -ENOBUFS;
-		}
-
-		memcpy(reply, cmd_pkt, 8);
-		reply[0] = COMMAND_PING_REPLY;
-		return 8;
+		net_buf_simple_add_u8(tx, COMMAND_PING_REPLY);
+		net_buf_simple_add_u8(tx, 1);
+		net_buf_simple_add_be16(tx, 0);
+		/* PING id */
+		net_buf_simple_add_be32(tx, net_buf_simple_pull_be32(rx));
+		return 0;
 
 	case COMMAND_INVITATION:
-		// TODO: add non-empty endpoint name and product id
-		if (reply_len < 4) {
-			LOG_WRN("Not enough buffer space for INVITATION_REPLY");
-			return -ENOBUFS;
-		}
-		reply[0] = COMMAND_INVITATION_REPLY_ACCEPTED;
-		memset(&reply[1], 0, 3);
-		return 4;
+		/* Peer EP name & product instance ID */
+		net_buf_simple_pull(rx, payload_len_words);
+		net_buf_simple_add_u8(tx, COMMAND_INVITATION_REPLY_ACCEPTED);
+		net_buf_simple_add_le24(tx, 0);
+		return 0;
 
 	case COMMAND_UMP_DATA:
-		if (cmd_pkt[1] < 1 || cmd_pkt[1] > 4) {
-			LOG_ERR("Invalid UMP payload length (%d)", cmd_pkt[1]);
-			return 0;
-		}
-		words = (const uint32_t *) &cmd_pkt[4];
-		for (size_t i=0; i<cmd_pkt[1]; i++) {
-			ump.data[i] = sys_be32_to_cpu(words[i]);
+		if (payload_len_words < 1 || payload_len_words > 4) {
+			LOG_ERR("Invalid UMP length");
+			return -1;
 		}
 
-		if (UMP_NUM_WORDS(ump) != cmd_pkt[1]) {
-			LOG_ERR("Invalid packet size fot MT=%02X", UMP_MT(ump));
-			return 0;
+		for (size_t i=0; i<payload_len_words; i++) {
+			ump.data[i] = net_buf_simple_pull_be32(rx);
+		}
+
+		if (UMP_NUM_WORDS(ump) != payload_len_words) {
+			LOG_ERR("Invalid UMP payload size for its message type");
+			return -1;
 		}
 
 		if (ep->rx_packet_cb){
@@ -57,7 +70,8 @@ static int udp_midi_dispatch_command_packet(struct udp_midi_ep *ep, const uint8_
 		return 0;
 
 	default:
-		LOG_WRN("Unknown command code %02X", cmd_pkt[0]);
+		LOG_WRN("Unknown command code %02X", cmd_code);
+		net_buf_simple_pull(rx, payload_len);
 		return 0;
 	}
 	return 0;
@@ -66,57 +80,53 @@ static int udp_midi_dispatch_command_packet(struct udp_midi_ep *ep, const uint8_
 static void udp_midi_rx_do_work(struct k_work *work)
 {
 	int ret;
-	size_t rx_len, rx_off, tx_off, cmd_pkt_len;
-	uint8_t rx_buf[BUFSIZE], tx_buf[BUFSIZE] = "MIDI";
 	struct sockaddr peer_addr;
 	socklen_t peer_addr_len;
 	struct udp_midi_ep *ep = CONTAINER_OF(work, struct udp_midi_ep, rx_work);
+	struct net_buf_simple *rxbuf = NET_BUF_SIMPLE(BUFSIZE);
+	struct net_buf_simple *txbuf = NET_BUF_SIMPLE(BUFSIZE);
 
+	net_buf_simple_init(rxbuf, 0);
+	net_buf_simple_init(txbuf, 0);
+
+	/* Receive packet */
 	LOG_DBG("Waiting for new UDP packet...");
 
-	ret = zsock_recvfrom(ep->sock, rx_buf, sizeof(rx_buf), 0, &peer_addr, &peer_addr_len);
-	if (ret <= 0) {
+	ret = zsock_recvfrom(ep->sock, rxbuf->data, rxbuf->size, 0, &peer_addr, &peer_addr_len);
+	if (ret < 0) {
 		LOG_ERR("Rx error: %d (%d)", ret, errno);
 		goto end;
 	}
+	rxbuf->len = ret;
 
-	rx_len = ret;
-	LOG_HEXDUMP_DBG(rx_buf, ret, "Received UDP packet");
+	LOG_HEXDUMP_DBG(rxbuf->data, rxbuf->len, "Received UDP packet");
 
-	if (rx_len % 4) {
-		LOG_ERR("Misaligned packet, length is not a multiple of 4");
+	/* Check for magic header */
+	if (rxbuf->len < 4 || memcmp(rxbuf->data, "MIDI", 4) != 0) {
+		LOG_WRN("Not a MIDI packet");
 		goto end;
 	}
 
-	if (rx_len < 4 || memcmp(rx_buf, "MIDI", 4) != 0) {
-		LOG_WRN("Not a MIDI packet");
-	}
+	net_buf_simple_pull(rxbuf, 4);
+	net_buf_simple_add_mem(txbuf, "MIDI", 4);
 
-	rx_off = tx_off = 4;
-	while (rx_off < rx_len) {
-		cmd_pkt_len = 4 * rx_buf[rx_off + 1];
-		if (rx_off + 4 + cmd_pkt_len > rx_len) {
-			LOG_ERR("Incomplete command packet");
-			goto end;
-		}
+	/* Parse contained command packets */
+	while (
+		rxbuf->len >= 4 &&
+		udp_midi_dispatch_command_packet(ep, rxbuf, txbuf) == 0
+	);
 
-		ret = udp_midi_dispatch_command_packet(ep, &rx_buf[rx_off], 4 + cmd_pkt_len, &tx_buf[tx_off], sizeof(tx_buf) - tx_off);
-		if (ret < 0) {
-			break;
-		}
-
-		tx_off += ret;
-		rx_off += 4 + cmd_pkt_len;
-	}
-
-	if (tx_off > 4) {
-		ret = zsock_sendto(ep->sock, tx_buf, tx_off, 0, &peer_addr, peer_addr_len);
+	/* Send reply if non empty */
+	if (txbuf->len > 4) {
+		LOG_HEXDUMP_DBG(txbuf->data, txbuf->len, "Sending UDP packet...");
+		ret = zsock_sendto(ep->sock, txbuf->data, txbuf->len, 0, &peer_addr, peer_addr_len);
 		if (ret < 0) {
 			LOG_ERR("Unable to send reply: %d", errno);
 		}
 	}
 
-	end: k_work_submit(&ep->rx_work);
+	end:
+		k_work_submit(&ep->rx_work);
 }
 
 int udp_midi_ep_start(struct udp_midi_ep *ep,
@@ -160,5 +170,4 @@ void udp_midi_send(struct udp_midi_ep *ep, const struct midi_ump ump)
 		zsock_sendto(ep->sock, buf, 8+4*UMP_NUM_WORDS(ump), 0,
 			     &ep->peers[i].addr, ep->peers[i].addr_len);
 	}
-	LOG_WRN("Not implemented !");
 }
